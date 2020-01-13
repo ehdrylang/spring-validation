0696d6cb8a35c3085f8468e281393a5ddb2b7892
jax <jax@doople.net>
2020-01-13 16:11:04 +0900

    UserController에 ExceptionHandler 추가

--- src/main/java/com/tistory/jeongpro/book/controller/UserController.java
+++ src/main/java/com/tistory/jeongpro/book/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.tistory.jeongpro.book.controller;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.constraints.Email;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +22,11 @@ public class UserController {
 	@GetMapping("/users")
 	public String getUserInformationByRequestParameter(@RequestParam("id") @Email String id) {
 		return "world";
+	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(ConstraintViolationException.class)
+	public Object exception(Exception e) {
+		return e.getMessage();
 	}
 }
